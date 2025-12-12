e581f0446908b55a7fb438d710d4a5da4894cea9
Fujii <167102935+Fujiiww@users.noreply.github.com>
2025-12-11 22:48:27 -0300

    Implement JsonDatabase for JSON file handling
    Refactor JsonDatabase class to manage JSON data storage.

--- Infra/Data/Context/JsonDatabase.cs
+++ Infra/Data/Context/JsonDatabase.cs
@@ -1,6 +1,38 @@
-﻿namespace TreinamentosCorp.API.Infra.Data.Context
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using System.Text.Json;
+using TreinamentosCorp.API.Domain.Entities;
+
+namespace TreinamentosCorp.API.Infra.Database
 {
     public class JsonDatabase
     {
+        private readonly string _filePath;
+        private DatabaseModel _cache;
+
+        public JsonDatabase()
+        {
+            _filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Infra", "Data", "db.json");
+            Load();
+        }
+
+        public DatabaseModel Data => _cache;
+
+        private void Load()
+        {
+            if (!File.Exists(_filePath))
+                throw new Exception("Arquivo db.json não encontrado.");
+
+            var json = File.ReadAllText(_filePath);
+            _cache = JsonSerializer.Deserialize<DatabaseModel>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+        }
+
+        public void Save()
+        {
+            var json = JsonSerializer.Serialize(_cache, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_filePath, json);
+        }
     }
 }
